c5c0bfab1c3b7eced7b62769edf5332bc83e2c1e
suyiiyii <suyiiyii@gmail.com>
2024-07-18 10:04:21 +0800

    init

---
+++ src/main/java/top/suyiiyii/guavalearn/GuavaLearnApplication.java
@@ -0,0 +1,13 @@
+package top.suyiiyii.guavalearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuavaLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuavaLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/suyiiyii/guavalearn/GuavaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package top.suyiiyii.guavalearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuavaLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
